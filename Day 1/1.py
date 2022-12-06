cd90ef66ed478f1b90e556162d1fc71912fb912b
Jakob Mueller <jake61392@gmail.com>
2022-12-05 22:29:06 -0600

    Finish Day 1 and 2

---
+++ Day 1/1.py
@@ -0,0 +1,18 @@
+#################################################################################################
+# Jakob Mueller
+# Advent of Code 2022 - Day 2
+#################################################################################################
+
+######################################################
+# Calries carried by elves
+######################################################
+def main():
+	# top calorie elf
+	print(max(sum(map(int,i.split())) for i in open("./input.txt").read().split('\n\n')))
+
+	# sum of calories of the top 3 elves
+	print(sum(sorted(sum(map(int,i.split())) for i in open("./input.txt").read().split('\n\n'))[-3:]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Day 2/2.py
@@ -0,0 +1,73 @@
+#################################################################################################
+# Jakob Mueller
+# Advent of Code 2022 - Day 2
+#################################################################################################
+
+######################################################
+# Rock - Paper - Scissors elf eddition
+######################################################
+def main():
+	totalScore = 0
+
+	# part 1
+	for i in open("./input.txt", 'r'):
+		if 'X' in i:
+			totalScore = totalScore + 1
+			if 'A' in i:
+				totalScore = totalScore + 3
+			elif 'C' in i:
+				totalScore = totalScore + 6
+		elif 'Y' in i:
+			totalScore = totalScore + 2
+			if 'A' in i:
+				totalScore = totalScore + 6
+			elif 'B' in i:
+				totalScore = totalScore + 3
+		elif 'Z' in i:
+			totalScore = totalScore + 3
+			if 'B' in i:
+				totalScore = totalScore + 6
+			elif 'C' in i:
+				totalScore = totalScore + 3
+
+	print(totalScore)
+	
+	# part 2
+	totalScore = 0
+	for i in open("./input.txt", 'r'):
+		if 'X' in i: # need to lose
+			if 'A' in i:
+				# they chose rock (A) so we need to choose scissors (C)
+				totalScore = totalScore + 3
+			elif 'B' in i: 
+				# they chose paper (B) so we need to choose rock (A)
+				totalScore = totalScore + 1
+			elif 'C' in i:
+				# they chose scissors (C) so we need to choose paper (B)
+				totalScore = totalScore + 2
+		elif 'Y' in i: # need to draw
+			totalScore = totalScore + 3
+			if 'A' in i:
+				# they chose rock (A) so we need to choose rock (A)
+				totalScore = totalScore + 1
+			elif 'B' in i: 
+				# they chose paper (B) so we need to choose paper (B)
+				totalScore = totalScore + 2
+			elif 'C' in i:
+				# they chose scissors (C) so we need to choose scissors (C)
+				totalScore = totalScore + 3
+		elif 'Z' in i: # need to win
+			totalScore = totalScore + 6
+			if 'A' in i:
+				# they chose rock (A) so we need to choose paper (B)
+				totalScore = totalScore + 2
+			elif 'B' in i: 
+				# they chose paper (B) so we need to choose scissors (C)
+				totalScore = totalScore + 3
+			elif 'C' in i:
+				# they chose scissors (C) so we need to choose rock (A)
+				totalScore = totalScore + 1
+
+	print(totalScore)
+if __name__ == "__main__":
+    main()
